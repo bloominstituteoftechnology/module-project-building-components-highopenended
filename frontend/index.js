32ffa7730916048ba08833c294c5e95d36c67ad0
Matt Glenn <highopenended@gmail.com>
2024-06-04 19:45:47 -0400

    Completed Challenge

--- frontend/index.js
+++ frontend/index.js
@@ -4,7 +4,16 @@ function moduleProject3() {
 
   function buildNav(links) {
     //  ✨ do your magic here
-    return document.createElement('nav')
+    let returnNav=document.createElement('nav');
+    
+    links.forEach(link=>{
+      const newA=document.createElement('a');
+      newA.href=link.href;
+      newA.textContent=link.textContent;
+      newA.title=link.title;
+      returnNav.append(newA);
+    });
+    return returnNav
   }
 
   // ❗ DOM creation using your `buildNav` component (do not change):
@@ -20,6 +29,26 @@ function moduleProject3() {
 
   function buildLearnerCard(learner, languages) {
     //  ✨ do your magic here
+    let returnCard=document.createElement('div');
+    let pName=document.createElement('p');
+    let pID=document.createElement('p');
+    let pDoB=document.createElement('p');
+    let pFavLang=document.createElement('p');
+
+    let languageName = languages.filter(language=>language.id===learner.favLanguage)[0].name;
+
+    pName.textContent=learner.fullName;
+    pID.textContent=`Learner ID: ${learner.id}`;
+    pDoB.textContent=`Date of Birth: ${learner.dateOfBirth}`;
+    pFavLang.textContent=`Favorite Language: ${languageName}`;
+
+    returnCard.classList.add('learner-card');
+    returnCard.append(pName);
+    returnCard.append(pID);
+    returnCard.append(pDoB);
+    returnCard.append(pFavLang);
+
+    return returnCard;
   }
 
   {
@@ -40,13 +69,86 @@ function moduleProject3() {
       { id: 17, fullName: 'Daniel Castillo', dateOfBirth: '1995-11-05', favLanguage: 12 }
     ]
     //  ✨ do your magic here
+    let mySection = document.querySelector('section');
+
+    learners.forEach(learner=>{
+      let newLearnerCard=buildLearnerCard(learner, languages);
+      mySection.append(newLearnerCard);
+    });
+
+    let arrCards = document.querySelectorAll('.learner-card');
+
+    arrCards.forEach(card => {
+      card.addEventListener('click',()=>{
+        arrCards.forEach(card=>card.classList.remove('active'));
+        card.classList.add('active');
+        event.stopPropagation();
+      });      
+    });
   }
+
 
   // 👉 TASK 3 - Write a `buildFooter` component that returns a footer
 
   function buildFooter(footerData) {
     //  ✨ do your magic here
-    return document.createElement('footer')
+    let newFooter=document.createElement('footer');
+
+    let divCompanyInfo = document.createElement('div');
+    let pCompanyName = document.createElement('p');
+    let pCompanyAddress = document.createElement('p');
+    let pCompanyEmail = document.createElement('p');
+    let aCompanyEmail = document.createElement('a');
+
+    let divSocialMedia = document.createElement('div');
+    let aTwitter= document.createElement('a');
+    let aFacebook= document.createElement('a');
+    let aInstagram= document.createElement('a');
+
+    let divSignoff= document.createElement('div');
+
+
+
+    // Add all the values
+    divCompanyInfo.classList.add('company-info');
+    
+    pCompanyName.classList.add('company-name');
+    pCompanyName.textContent=footerData.companyName;
+    
+    pCompanyAddress.classList.add('address');
+    pCompanyAddress.textContent=footerData.address;
+
+    pCompanyEmail.classList.add('contact-email');
+    pCompanyEmail.textContent='Email: ';
+    aCompanyEmail.textContent=footerData.contactEmail;
+    aCompanyEmail.href="mailto:info@example.com";
+
+    aTwitter.textContent='Twitter';
+    aTwitter.href=footerData.socialMedia.twitter;
+
+    aFacebook.textContent='Facebook';
+    aFacebook.href=footerData.socialMedia.facebook;
+
+    aInstagram.textContent='Instagram';
+    aInstagram.href=footerData.socialMedia.instagram;
+
+    divSignoff.textContent=`© BLOOM INSTITUTE OF TECHNOLOGY ${new Date().getFullYear()}`;
+
+    // Append Everything
+    divCompanyInfo.append(pCompanyName);
+    divCompanyInfo.append(pCompanyAddress);
+    divCompanyInfo.append(pCompanyEmail);
+    pCompanyEmail.append(aCompanyEmail);
+
+    divSocialMedia.append(aTwitter);
+    divSocialMedia.append(aFacebook);
+    divSocialMedia.append(aInstagram);    
+
+    newFooter.append(divCompanyInfo);
+    newFooter.append(divSocialMedia);
+    newFooter.append(divSignoff);    
+
+    return newFooter;
   }
 
   // ❗ DOM creation using your `buildFooter` component (do not change):
@@ -62,8 +164,15 @@ function moduleProject3() {
   }))
 
   // 👉 TASK 4 - Clicking on the section should deactivate the active card
-
+  let section = document.querySelector('section');
+  section.addEventListener('click',unclickAll);
   //  ✨ do your magic here
+
+  function unclickAll(){    
+    let arrCards = document.querySelectorAll('.learner-card');
+    arrCards.forEach(card =>card.classList.remove('active'));
+  }
+
 }
 
 // ❗ DO NOT CHANGE THIS CODE
